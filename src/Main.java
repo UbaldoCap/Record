9aca25053d9af039f6d7bf775462b1f820e7f820
Ubaldo <ubaldocapalbo@gmail.com>
2023-12-08 16:50:55 +0100

    end

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Punto punto1 = new Punto(5, 6);
+        System.out.println(punto1);
+    }
+}
---
+++ src/Punto.java
@@ -0,0 +1,2 @@
+public record Punto(int x, int y) {
+}
